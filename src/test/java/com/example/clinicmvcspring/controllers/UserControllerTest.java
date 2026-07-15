e8acac715eedcdfbfdc3a4dec6500ddb213027b5
yousef abutaha <s12217011@stu.najah.edu>
2026-07-15 15:12:31 +0300

    Implement a test on getAllUsers endpoint for UserController

---
+++ src/test/java/com/example/clinicmvcspring/controllers/UserControllerTest.java
@@ -0,0 +1,60 @@
+package com.example.clinicmvcspring.controllers;
+
+
+import com.example.clinicmvcspring.models.AppUser;
+import com.example.clinicmvcspring.services.AppUserDetailsService;
+import com.example.clinicmvcspring.services.DoctorService;
+import com.example.clinicmvcspring.services.JwtService;
+import com.example.clinicmvcspring.services.RefreshTokenService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.Collections;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@WebMvcTest(UserController.class)
+@AutoConfigureMockMvc(addFilters = false) // disables Spring Security
+public class UserControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+//in mockMvc assert and act phases are together
+    @MockitoBean
+    private AppUserDetailsService userService;
+    @MockitoBean
+    private DoctorService doctorService;
+    @MockitoBean
+    private AuthenticationManager authenticationManager;
+    @MockitoBean
+    private JwtService jwtService;
+    @MockitoBean
+    private RefreshTokenService refreshTokenService;
+
+   // test get all users endpoint
+
+    @Test
+    public void UserController_GetAllUsers_ReturnsOk()throws Exception
+    {
+        // arrange
+        Page<AppUser> fakePage = new PageImpl<>(Collections.emptyList());
+        when(userService.getAllUsers(any())).thenReturn(fakePage);
+
+
+        // act and assert
+
+        // ACT & ASSERT
+        mockMvc.perform(get("/api/v1/users"))
+                .andExpect(status().isOk());
+
+    }
+}
