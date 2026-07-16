fcbc4ece240291545a74d0683a93602d7a004333
yousef abutaha <s12217011@stu.najah.edu>
2026-07-16 12:40:18 +0300

    Implement Login Endpoint Test

--- src/test/java/com/example/clinicmvcspring/controllers/UserControllerTest.java
+++ src/test/java/com/example/clinicmvcspring/controllers/UserControllerTest.java
@@ -1,11 +1,10 @@
 package com.example.clinicmvcspring.controllers;
 
-import com.example.clinicmvcspring.models.AppUser;
-import com.example.clinicmvcspring.services.AppUserDetailsService;
-import com.example.clinicmvcspring.services.DoctorService;
-import com.example.clinicmvcspring.services.JwtService;
-import com.example.clinicmvcspring.services.RefreshTokenService;
+import java.util.Collections;
+
 import org.junit.jupiter.api.Test;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
@@ -15,16 +14,20 @@ import org.springframework.http.MediaType;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import java.util.Collections;
+import com.example.clinicmvcspring.models.AppUser;
+import com.example.clinicmvcspring.models.Role;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
+import com.example.clinicmvcspring.models.CustomUserDetails;
+import com.example.clinicmvcspring.models.RefreshToken;
+import com.example.clinicmvcspring.services.AppUserDetailsService;
+import com.example.clinicmvcspring.services.DoctorService;
+import com.example.clinicmvcspring.services.JwtService;
+import com.example.clinicmvcspring.services.RefreshTokenService;
 
 @WebMvcTest(UserController.class)
 @AutoConfigureMockMvc(addFilters = false) // disables Spring Security
@@ -82,15 +85,48 @@ public class UserControllerTest {
         mockMvc.perform(post("/api/v1/users/signup")
                 .contentType(MediaType.APPLICATION_JSON)// req body of post in perform
                 .content("""
-                {
-                    "username": "name",
-                    "password": "1234",
-                    "role": "DOCTOR"
-                }
-                """)//JSON BODY REQ
-            )
+                        {
+                            "username": "name",
+                            "password": "1234",
+                            "role": "DOCTOR"
+                        }
+                        """)// JSON BODY REQ
+        )
                 .andExpect(status().is(201))
                 .andExpect(jsonPath("$.message").value("User registered successfully!"));
+
+    }
+
+    // Test generate Access token
+
+    @Test
+    public void UserController_login_GenerateTokenSuccessfully() throws Exception {
+        AppUser fakeAppUser = new AppUser();
+        fakeAppUser.setUsername("fake-name");
+        fakeAppUser.setId(55);
+        fakeAppUser.setRole(Role.DOCTOR);
+        CustomUserDetails fakeUserDetails = new CustomUserDetails(fakeAppUser);
+        RefreshToken fakeRefreshToken = new RefreshToken();
+        fakeRefreshToken.setPlainTextToken("fake-refresh-token");
+
+        when(userService.loadUserByUsername("fake-name")).thenReturn(fakeUserDetails);
+        when(refreshTokenService.createRefreshToken("fake-name")).thenReturn(fakeRefreshToken);
+        when(jwtService.generateToken(fakeUserDetails)).thenReturn("fake-jwt-token");
+
+        // act + assert
+
+        mockMvc.perform(post("/api/v1/users/login")
+                .contentType(MediaType.APPLICATION_JSON)// req body of post in perform
+                .content("""
+                        {
+                            "username": "fake-name",
+                            "password": "1234"
+                        }
+                        """)// JSON BODY REQ
+        )
+                .andExpect(status().is(200))
+                .andExpect(jsonPath("$.token").value("fake-jwt-token"))
+                .andExpect(jsonPath("$.refreshToken").value("fake-refresh-token"));
 
     }
 
