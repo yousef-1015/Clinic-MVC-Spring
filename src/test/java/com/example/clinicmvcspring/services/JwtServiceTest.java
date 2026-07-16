f1344ab2c5b119d6f0ea9801ce1d7cbf5fffd553
yousef abutaha <s12217011@stu.najah.edu>
2026-07-16 13:26:04 +0300

    Add JwtServiceTest with testUsrname and TestTokenCreation

---
+++ src/test/java/com/example/clinicmvcspring/services/JwtServiceTest.java
@@ -0,0 +1,61 @@
+package com.example.clinicmvcspring.services;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import com.example.clinicmvcspring.models.AppUser;
+import com.example.clinicmvcspring.models.CustomUserDetails;
+import com.example.clinicmvcspring.models.Role;
+
+@ExtendWith(MockitoExtension.class)
+public class JwtServiceTest {
+
+    @BeforeEach // for each test create these new object
+    void setUp() {
+        jwtService = new JwtService();
+
+        ReflectionTestUtils.setField(jwtService, "jwtSecret",
+                "339b66b1e4668bca8357465af57f141f538f6def6abb1b7c7d5fce7173bbfb67");
+    }
+
+    private JwtService jwtService;
+
+    @Test
+    void JwtService_generateToken_TokenNotEmpty() {
+        // Arrange
+        AppUser fakeAppUser = new AppUser();
+        fakeAppUser.setUsername("fake-name");
+        fakeAppUser.setId(55);
+        fakeAppUser.setRole(Role.DOCTOR);
+        CustomUserDetails fakeUserDetails = new CustomUserDetails(fakeAppUser);
+
+        // act
+        String token = jwtService.generateToken(fakeUserDetails);
+
+        // assert
+        assertThat(token).isNotEmpty();
+    }
+
+    // arrange
+    // act
+    // assert
+    @Test
+    void JwtService_generateToken_TokenContainsUsername() {
+        // Arrange
+        AppUser fakeAppUser = new AppUser();
+        fakeAppUser.setUsername("fake-name");
+        fakeAppUser.setId(55);
+        fakeAppUser.setRole(Role.DOCTOR);
+        CustomUserDetails fakeUserDetails = new CustomUserDetails(fakeAppUser);
+        String token = jwtService.generateToken(fakeUserDetails);
+
+        // act
+        String extractedUsername = jwtService.extractUsername(token);
+        // assert
+        assertThat(extractedUsername).isEqualTo(fakeUserDetails.getUsername());
+    }
+}
