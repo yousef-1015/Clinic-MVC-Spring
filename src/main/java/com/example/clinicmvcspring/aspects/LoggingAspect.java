dc9f8ff023dc4b055d7b30386d16fc86f87271e3
yousef abutaha <s12217011@stu.najah.edu>
2026-07-20 11:07:30 +0300

    Implement Logging for Exceptions Caused by Service methods

--- src/main/java/com/example/clinicmvcspring/aspects/LoggingAspect.java
+++ src/main/java/com/example/clinicmvcspring/aspects/LoggingAspect.java
@@ -2,6 +2,7 @@ package com.example.clinicmvcspring.aspects;
 
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -20,11 +21,20 @@ public class LoggingAspect {
 
     }
 
+    // no crashes
     @AfterReturning(pointcut = "execution(* com.example.clinicmvcspring.services.*.*(..))", returning = "result")
     public void logAfterSuccessfulReturn(JoinPoint joinPoint, Object result) {
 
         log.info("Service method {" + joinPoint.getSignature().getName()
                 + "} finished successfully and returned:\n Data {" + result.toString() + "}");
+
+    }
+
+    // exception where thrown
+    @AfterThrowing(pointcut = "execution(* com.example.clinicmvcspring.services.*.*(..))", throwing = "error")
+    public void LogAfterExceptions(JoinPoint joinPoint, Exception error) {
+        log.error("Service method {" + joinPoint.getSignature().getName()
+                + "} Threw an Exception:[" + error.getMessage() + "]");
 
     }
 
