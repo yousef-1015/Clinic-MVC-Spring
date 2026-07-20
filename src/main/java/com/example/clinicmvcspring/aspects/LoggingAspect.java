38d5ae04b0e9b2ae9811ca3bf9aeefd9f8e64d83
yousef abutaha <s12217011@stu.najah.edu>
2026-07-20 13:54:57 +0300

    Adding Class name and parameters in the logging

--- src/main/java/com/example/clinicmvcspring/aspects/LoggingAspect.java
+++ src/main/java/com/example/clinicmvcspring/aspects/LoggingAspect.java
@@ -1,5 +1,6 @@
 package com.example.clinicmvcspring.aspects;
 
+import java.lang.reflect.Array;
 import java.time.Instant;
 
 import org.aspectj.lang.JoinPoint;
@@ -17,28 +18,41 @@ import lombok.extern.slf4j.Slf4j;
 @Aspect
 @Component
 public class LoggingAspect {
+    private String getParametersAsString(JoinPoint joinPoint) {
+        Object[] params = joinPoint.getArgs();
+        return java.util.Arrays.toString(params);
+    }
 
     // Advice
     @Before("execution(* com.example.clinicmvcspring.services.*.*(..))")
     public void beginLogging(JoinPoint joinPoint) {
-        log.info("Service method " + joinPoint.getSignature().getName() + " was called");
+        String paramString = getParametersAsString(joinPoint);
+
+        log.info("Service method {" + joinPoint.getSignature().getName() + "} with the parameters("
+                + paramString + ") from class " + joinPoint.getTarget().getClass().getSimpleName()
+                + "was called");
 
     }
 
     // no crashes
     @AfterReturning(pointcut = "execution(* com.example.clinicmvcspring.services.*.*(..))", returning = "result")
     public void logAfterSuccessfulReturn(JoinPoint joinPoint, Object result) {
+        String paramString = getParametersAsString(joinPoint);
 
-        log.info("Service method {" + joinPoint.getSignature().getName()
-                + "} finished successfully and returned:\n Data {" + result.toString() + "}");
+        log.info("Service method {" + joinPoint.getSignature().getName() + "} with the parameters("
+                + paramString + ") from class " + joinPoint.getTarget().getClass().getSimpleName()
+                + "finished successfully and returned [" + result.toString() + "]");
 
     }
 
     // exception where thrown
     @AfterThrowing(pointcut = "execution(* com.example.clinicmvcspring.services.*.*(..))", throwing = "error")
     public void LogAfterExceptions(JoinPoint joinPoint, Exception error) {
-        log.error("Service method {" + joinPoint.getSignature().getName()
-                + "} Threw an Exception:[" + error.getMessage() + "]");
+        String paramString = getParametersAsString(joinPoint);
+
+        log.error("Service method {" + joinPoint.getSignature().getName() + "} with the parameters("
+                + paramString + ") from class " + joinPoint.getTarget().getClass().getSimpleName()
+                + " threw an exception: {" + error.getMessage() + "}");
 
     }
 
@@ -56,7 +70,8 @@ public class LoggingAspect {
         long elapsedTime = endTime - startTime;
 
         log.info("Service method {" + joinPoint.getSignature().getName()
-                + "} started at(" + Instant.ofEpochMilli(startTime) + ") and finished at ("
+                + "}" + " from class " + joinPoint.getTarget().getClass().getSimpleName() + " started at("
+                + Instant.ofEpochMilli(startTime) + ") and finished at ("
                 + Instant.ofEpochMilli(endTime) + ") time taken: (" + elapsedTime
                 + " ms)");
 
@@ -76,7 +91,8 @@ public class LoggingAspect {
         long elapsedTime = endTime - startTime;
 
         log.info("controller endpoint {" + joinPoint.getSignature().getName()
-                + "} started at(" + Instant.ofEpochMilli(startTime) + ") and finished at ("
+                + "}" + " from class " + joinPoint.getTarget().getClass().getSimpleName() + "  started at("
+                + Instant.ofEpochMilli(startTime) + ") and finished at ("
                 + Instant.ofEpochMilli(endTime) + ") time taken: (" + elapsedTime
                 + " ms)");
 
