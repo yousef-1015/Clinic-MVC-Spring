3d95b81ca01649931e4895e8be58c5dadc23cadb
yousef abutaha <s12217011@stu.najah.edu>
2026-08-02 14:57:45 +0300

    Make Queue and Binding fo every

--- src/main/java/com/example/clinicmvcspring/config/RabbitMQConfig.java
+++ src/main/java/com/example/clinicmvcspring/config/RabbitMQConfig.java
@@ -2,8 +2,8 @@ package com.example.clinicmvcspring.config;
 
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.FanoutExchange;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
@@ -12,24 +12,68 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RabbitMQConfig {
 
-    // THE QUEUE
+    // THE QUEUES
     @Bean
-    public Queue doctorQueue() {
-        return new Queue("doctor.queue", true);
+    public Queue doctorCreatedQueue() {
+        return new Queue("doctor.created.queue", true);
+
+    }
+
+    @Bean
+    public Queue doctorUpdatedQueue() {
+        return new Queue("doctor.updated.queue", true);
+
+    }
+
+    @Bean
+    public Queue doctorDeletedQueue() {
+        return new Queue("doctor.deleted.queue", true);
+
+    }
+
+    @Bean
+    public Queue userLoggedInQueue() {
+        return new Queue("user.loggedIn.queue", true);
+
     }
 
     // THE EXCHANGE
     @Bean // broadcast
-    public FanoutExchange doctorExchange() {
-        return new FanoutExchange("doctor.exchange");
+    public TopicExchange doctorExchange() {
+        return new TopicExchange("clinic.events");
     }
 
-    // THE BINDING
+    // THE BINDINGS
     @Bean
-    public Binding binding(Queue docQueue, FanoutExchange docExchange) {
+    public Binding doctorCreatedBinding(Queue doctorCreatedQueue, TopicExchange docExchange) {
         return BindingBuilder
-                .bind(docQueue)
-                .to(docExchange);
+                .bind(doctorCreatedQueue)
+                .to(docExchange)
+                .with("doctor.created");// the routing key
+    }
+
+    @Bean
+    public Binding doctorUpdatedBinding(Queue doctorUpdatedQueue, TopicExchange docExchange) {
+        return BindingBuilder
+                .bind(doctorUpdatedQueue)
+                .to(docExchange)
+                .with("doctor.updated");// the routing key
+    }
+
+    @Bean
+    public Binding doctorDeletedBinding(Queue doctorDeletedQueue, TopicExchange docExchange) {
+        return BindingBuilder
+                .bind(doctorDeletedQueue)
+                .to(docExchange)
+                .with("doctor.deleted");// the routing key
+    }
+
+    @Bean
+    public Binding userLoggedInBinding(Queue userLoggedInQueue, TopicExchange docExchange) {
+        return BindingBuilder
+                .bind(userLoggedInQueue)
+                .to(docExchange)
+                .with("user.loggedin");// the routing key
     }
 
     @Bean
